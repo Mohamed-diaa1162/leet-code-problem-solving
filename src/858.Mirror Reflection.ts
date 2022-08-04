2e6449c8c3593fdf1243e500f31ef71918d2e32a
unknown <mohameddya20@gmail.com>
2022-08-05 01:45:26 +0200

    Mirror Reflection

---
+++ src/858.Mirror Reflection.ts
@@ -0,0 +1,15 @@
+// #solved
+// problem link https://leetcode.com/problems/mirror-reflection/
+function mirrorReflection(p: number, q: number): number {
+  while (p % 2 == 0 && q % 2 == 0) {
+    // p & q are both even, divide both by 2
+    p = p / 2;
+    q = q / 2;
+  }
+  if (p % 2 == 0) return 2; // p is even
+  else if (q % 2 == 0) return 0; // q is even
+  else return 1; // p & q are odd
+}
+
+console.log(mirrorReflection(2, 1)); //Output 2
+console.log(mirrorReflection(3, 1)); //Output 1
